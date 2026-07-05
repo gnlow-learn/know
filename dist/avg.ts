0f45633a543899e63a84029a58d96b6ca3a89362
Gnlow <gnlow@pm.me>
2026-07-05 16:49:18 +0900

    avg.ts

---
+++ dist/avg.ts
@@ -0,0 +1,11 @@
+import { text } from "https://gnlow.dev/csvore@0.2.1"
+import * as z from "https://esm.sh/zod@4.1.13"
+import csv from "./avg.csv" with { type: "text" }
+
+export default text(csv).csv().parseRow(z.object({
+    job: z.string(),
+    popM: z.coerce.number(),
+    popF: z.coerce.number(),
+    peakAge: z.coerce.number(),
+    meanAge: z.coerce.number(),
+})).raw.toArray()
